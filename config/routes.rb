9c104b53e3b3a97bf7cffccca53acbbb0fd70359
Nguyen Dinh Minh <minh.ngdi@gmail.com>
2019-03-06 16:24:52 +0700

    refs rOrTuts #chapter_3 Finish with i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root "static_pages#home"
+    get "static_pages/help"
+    get "static_pages/about"
+    get "static_pages/contact"
+  end
 end
