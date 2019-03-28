a40f761fcd597b58cefd5cb031605a06635a5fd9
Nguyen Dinh Minh <minh.ngdi@gmail.com>
2019-03-29 00:17:38 +0700

    refs rOrTuts #chapter_10 Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
+  before_action :find_user, only: [:show, :create, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
-    return if @user
-    flash[:danger] = t ".show.notfound"
-    redirect_to root_path
+  def index
+    @users = User.paginate(page: params[:page], per_page: Settings.per_page_count)
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -23,10 +26,50 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = t ".update.messages.success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find_by(id: params[:id]).destroy
+    flash[:success] = t ".user_deleted"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".not_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to(root_url) unless current_user.current_user? @user
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+
+    return if @user
+    flash[:danger] = t ".show.notfound"
+    redirect_to root_url
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
